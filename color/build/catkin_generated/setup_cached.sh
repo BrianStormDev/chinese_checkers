f95d829ebb36ae3d184754b0ac18eb57cfb3235e
Brian_Leong <bleong314@berkeley.edu>
2024-11-16 15:10:52 -0800

    Added color_sorting package

---
+++ color/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa24/class/ee106a-adh/color_sorter/color/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/cc/ee106a/fa24/class/ee106a-adh/color_sorter/color/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cc/ee106a/fa24/class/ee106a-adh/color_sorter/color/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa24/class/ee106a-adh/color_sorter/color/src:$ROS_PACKAGE_PATH"
