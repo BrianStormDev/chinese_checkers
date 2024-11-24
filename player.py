b6b0d5d74e68b411c4e25acbbb7e91c015020c6b
bleong314 <bleong314@berkeley.edu>
2024-11-23 22:50:37 -0800

    Working on Peg and Player classes
    Added a method that can initialize the pegs for a player

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import numpy as np
-import Point
-import Peg
+from Point import Point
+from Peg import Peg
 
 class Player:
     def __init__(self, color, number, directions, origin):
@@ -10,10 +10,6 @@ class Player:
         number: integer indicating the number of the player
         directions: an array of strings representing coordinate movements with respect to each player
         pieces: an array of Pegs representing the pegs a player occupies
-
-        Each player should also have a triangle start associated with them 
-        Each player should also have a triangle end associated with them
-        Each player should also have an array of pegs associated with them
         """
         self.color = color
         self.number = number
@@ -32,15 +28,16 @@ class Player:
     def initialize_pegs(self, origin):
         """
         Initializes the pegs and their position
-        origin: tuple representing the origin of the positions
+        origin: Point representing the origin of the positions
         """
         self.pegs = []
-        for i in range(3):
-            for j in range(i - 1):
+        for i in range(4):
+            for j in range(0, i + 1):
                 cur_position = origin + self.directions["UL"] * i + self.directions["R"] * j
                 cur_peg = Peg(cur_position, True, False, self.color)
                 self.pegs.append(cur_peg)
-    
+        return self.pegs
+        
     def reset_pegs(self):
         self.current_pegs = self.initial_pegs.copy()
 
@@ -52,3 +49,8 @@ purple_directions = [Point(-1, -1), Point(-2, 0), Point(-1, 1), Point(1, 1), Poi
 green_directions = [Point(-2, 0), Point(-1, 1), Point(1, 1), Point(2, 0), Point(1, -1), Point(-1, -1)]
 
 player_1 = Player("Red", 1, red_directions, Point(0, 17))
+player_2 = Player("Orange", 2, orange_directions, Point(0, 5))
+player_3 = Player("Blue", 3, blue_directions, Point(0, 12))
+player_4 = Player("Yellow", 4, yellow_directions, Point(16, 17))
+player_5 = Player("Purple", 5, purple_directions, Point(25, 12))
+player_6 = Player("Green", 6, green_directions, Point(25, 5))
