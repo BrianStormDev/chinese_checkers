a87991a6a5a73297dcff237579f415b9d5ca6b66
BrianStormDev <gameboy0429@gmail.com>
2024-12-07 10:17:35 -0800

    File Changes

---
+++ build/ar_tag/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa24/class/ee106a-air/ros_workspaces/chinese_checkers/src/ar_tag
+# Build directory: /home/cc/ee106a/fa24/class/ee106a-air/ros_workspaces/chinese_checkers/build/ar_tag
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ game/src/Peg.py
@@ -0,0 +1,32 @@
+from Point import Point
+
+class Peg:
+    def __init__(self, position: Point, color: str, in_board: bool, is_empty: bool) -> None:
+        """
+        Initialize a peg.
+        position: Point indicating the position of the peg
+        color: string indicating the color of the peg
+        in_board: boolean indicating if the peg is a slot on the board
+        is_empty: boolean indicating if the peg is empty
+        """
+        self.position = position
+        self.color = color
+        self.in_board = in_board
+        self.is_empty = is_empty
+        
+    def copy(self):
+        return Peg(self.position, self.color, self.in_board, self.is_empty)
+    
+    def __repr__(self):
+        position_str = f'Position: {str(self.position)} | '
+        color_str = f'Color: {self.color} | '
+        in_board = f'In Board: {self.in_board} | '
+        is_empty = f'Is Empty: {self.is_empty} '
+        return position_str + color_str + in_board + is_empty
+    
+    def __str__(self):
+        position_str = f'Position: {str(self.position)} | '
+        color_str = f'Color: {self.color} | '
+        in_board = f'In Board: {self.in_board} | '
+        is_empty = f'Is Empty: {self.is_empty} '
+        return position_str + color_str + in_board + is_empty
