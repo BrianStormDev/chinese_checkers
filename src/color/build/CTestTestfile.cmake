821a4e4e17e0594577cb64865f94a262e8f6cdba
Joey-Tai <joeyphung03@gmail.com>
2024-12-09 20:51:17 -0800

    minor changes to internal package

--- src/color/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106a/fa24/class/ee106a-adh/color_sorter/color/src
-# Build directory: /home/cc/ee106a/fa24/class/ee106a-adh/color_sorter/color/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("color_detection")
