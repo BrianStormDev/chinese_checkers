44a9bc93f095b8276e2728655a64eafbcf59f63b
Benji Xu <benji.xu@berkeley.edu>
2024-12-10 14:42:04 -0800

    added new homography for data collection, commit to switch branch

---
+++ src/internal/src/cv/new_rectification.py
@@ -0,0 +1,27 @@
+import sys
+import cv2
+from rectify import rectify_image
+from point_reader import scale_points
+from point_picker import pick_points
+import numpy as np
+
+def main():
+
+    i = 11
+
+    img_path = f'../../../ar_tag/src/saved_images/new_image_{i + 6}.jpg'
+    width = 500
+    height = 500
+    points = np.array(pick_points(img_path))
+
+    img = cv2.imread(img_path)
+    img_dim = (img.shape[1], img.shape[0])
+    img_pts = scale_points(points, img_dim)
+    rectified_dim = (width, height)
+    rectified_img = rectify_image(img, img_pts, rectified_dim)
+    cv2.imwrite(f"new_rectified_images/image_{i}.jpg", rectified_img)
+    cv2.imshow("window", rectified_img)
+    cv2.waitKey()
+
+if __name__ == "__main__":
+    main()
