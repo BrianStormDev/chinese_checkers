cea7e7226336dee604d3c75b8b7facff8f4900eb
Joey-Tai Phung <joeyphung03@gmail.com>
2024-12-12 18:10:24 -0800

    helper functions and adjusted movecommander code

---
+++ src/sawyer_full_stack/calibration_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+import tf2_ros
+
+points = []
+
+def listener():
+    tfBuffer = tf2_ros.Buffer()
+    tfListener = tf2_ros.TransformListener(tfBuffer)
+    while not rospy.is_shutdown():
+        try: 
+            trans = tfBuffer.lookup_transform("base", "right_gripper_tip", rospy.Time())
+            print(trans)
+            user = input("Enter y to save, n to stop: ")
+            if user == "y":
+                 points.append(trans.transform.translation)
+            elif user == "n":
+                 break
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            print("error")
+    # Do some path on the points
+    # Print the calibrated values
+
+if __name__ == '__main__':
+	rospy.init_node('listener', anonymous=True)
+	listener()
