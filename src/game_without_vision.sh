503ecfaad5f841f735e1fe8bb25069c4be0859d6
Joey-Tai <joeyphung03@gmail.com>
2024-12-13 22:12:14 -0800

    some changes

--- src/game_without_vision.sh
+++ src/game_without_vision.sh
@@ -3,5 +3,5 @@ mate-terminal \
     --tab --title="joint_action_server" -- bash -c "rosrun intera_interface joint_trajectory_action_server.py; exec bash" \
     --tab --title="sawyer_config" -- bash -c "roslaunch sawyer_moveit_config sawyer_moveit.launch electric_gripper:=true; exec bash" \
     --tab --title="ar_tag_detect" -- bash -c "roslaunch sawyer_full_stack sawyer_camera_track.launch; exec bash" \
+    --tab --title="dummy_main.py" -- bash -c "python src/internal/dummy_main.py; exec bash" \
     --tab --title="main.py" -- bash -c "python src/sawyer_full_stack/main.py; exec bash"
-wait 
