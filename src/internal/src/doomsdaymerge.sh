3081783d9bf697d2202159b74aeeb3c2eaac6d83
Benji Xu <benji.xu@berkeley.edu>
2024-12-13 20:45:17 -0800

    tried fixing launch file

---
+++ src/internal/src/doomsdaymerge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /home/tmp/ee106a-air/myenv/bin/activate
+source ~ee106a/sawyer_setup.bash
+catkin_make
+source devel/setup.bash
+
+THIS FILE IS USELESS NOW HAHA
+
+DOOMSDAY MERGE!
